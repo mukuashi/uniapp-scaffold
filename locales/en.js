347ded57b1a4dcb0607e2bb90eccc76b893fb0fd
mukuashi <oncwnuIRIBtgrEF91mBuy542EFWI@git.weixin.qq.com>
2020-04-13 17:11:56 +0800

    add国际化配置

---
+++ locales/en.js
@@ -0,0 +1,8 @@
+// 英文
+export default {
+  name: "Asako Daily",
+  brand: "PhotoArtLife",
+  abstract:
+    "A selection of global visual and photographic art creations, a glimpse of all the heroes in the world, and let the AI daily newspaper accompany you every day ☀️",
+  copyright: "@Asako Studio ✘ PhotoArtLife ✘ MUX Lab"
+};
---
+++ locales/zh-CN.js
@@ -0,0 +1,8 @@
+// 中文
+export default {
+  name: "浅子影报",
+  brand: "PhotoArtLife",
+  abstract:
+    "一页日历 • AI日报 伴你每天好心情 ☀️\n精选全球视觉与摄影艺术创作，览尽天下英雄。",
+  copyright: "@浅子文化工作室 ✘ PhotoArtLife ✘ MUX Lab"
+};
